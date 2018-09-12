3ec177fab79db9e65c0e3e7bf5bda74cd08e644e
lordofxtreme <kingfahad0802@gmail.com>
2018-09-13 01:42:32 +0600

    oa about incomplete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,20 +13,6 @@
 	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
 	<meta name="author" content="FREEHTML5.CO" />
 
-  <!--
-	//////////////////////////////////////////////////////
-
-	FREE HTML5 TEMPLATE
-	DESIGNED & DEVELOPED by FREEHTML5.CO
-
-	Website: 		http://freehtml5.co/
-	Email: 			info@freehtml5.co
-	Twitter: 		http://twitter.com/fh5co
-	Facebook: 		https://www.facebook.com/fh5co
-
-	//////////////////////////////////////////////////////
-	 -->
-
   	<!-- Facebook and Twitter integration -->
 	<meta property="og:title" content=""/>
 	<meta property="og:image" content=""/>
@@ -103,20 +89,53 @@
 			<div class="heading-section text-center">
 				<h2>Who we are</h2>
 			</div>
-			<div class="row">
+			<div class="row about">
 				<div class="col-md-10 col-md-offset-1">
-					<div class="col-md-6 col-sm-6 services-num services-num-text-right">
-						<span class="number-holder">01</span>
-						<div class="desc">
-							<h3>Countries Vokalia and Consonantia</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+					<img class="img-responsive" src="images/about.jpg" alt="About">
+				</div>
+				<div class="col-md-10 col-md-offset-1">
+					<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen.</p>
+					<blockquote>
+					  <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+					</blockquote>
+					<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen.</p>
+				</div>
+			</div>
+			<div class="heading-section text-center">
+				<h2>Our Team</h2>
+			</div>
+			<div class="row about">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="col-md-6 col-sm-6">
+						<div class="team-section-grid" style="background-image: url(img/anis.jpg);">
+							<div class="overlay-section">
+								<div class="desc">
+									<h3>Anisul Islam</h3>
+									<span class="position">Co-Founder</span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p class="fh5co-social-icons">
+										<a href="#"><i class="icon-twitter-with-circle"></i></a>
+										<a href="#"><i class="icon-facebook-with-circle"></i></a>
+										<a href="#"><i class="icon-instagram-with-circle"></i></a>
+									</p>
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="col-md-6 col-sm-6 services-num">
-						<span class="number-holder">02</span>
-						<div class="desc">
-							<h3>Countries Vokalia and Consonantia</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+					<div class="col-md-6 col-sm-6">
+						<div class="team-section-grid" style="background-image: url(images/author-4.jpg);">
+							<div class="overlay-section">
+								<div class="desc">
+									<h3>John Doe</h3>
+									<span class="position">Chief Executive Officer</span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p class="fh5co-social-icons">
+										<a href="#"><i class="icon-twitter-with-circle"></i></a>
+										<a href="#"><i class="icon-facebook-with-circle"></i></a>
+										<a href="#"><i class="icon-instagram-with-circle"></i></a>
+									</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -141,82 +160,133 @@
 			<div class="heading-section text-center">
 				<h2>Our Services</h2>
 			</div>
-			<div class="row">
-				<div class="col-md-4 col-sm-6">
-					<div class="fh5co-services-right">
-						<div class="fh5co-table2 text-center">
-							<div class="fh5co-table-cell2">
-								<i class="icon-heart3"></i>
-							</div>
-						</div>
-						<div class="holder-section">
-							<h3>Crafted With Love</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
-						</div>
-					</div>
-					<div class="fh5co-services-right">
-						<div class="fh5co-table2 fh5co-table2-color-2 text-center">
-							<div class="fh5co-table-cell2">
+			<div id="fh5co-services-section">
+				<div class="container">
+					<div class="row text-center">
+						<div class="col-md-4">
+							<div class="services-grid">
 								<i class="icon-laptop"></i>
+								<h3>Software Development</h3>
+								<p>We love bringing our concepts to life using a variety of languages and platforms. We deliver a final product that prioritizes maintainability.</p>
 							</div>
 						</div>
-						<div class="holder-section">
-							<h3>Web Design</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
-						</div>
-					</div>
-					<div class="fh5co-services-right">
-						<div class="fh5co-table2 fh5co-table2-color-3 text-center">
-							<div class="fh5co-table-cell2">
-								<i class="icon-video"></i>
+						<div class="col-md-4">
+							<div class="services-grid">
+								<i class="icon-html-five2"></i>
+								<h3>Web Development</h3>
+								<p>We design & develop website for the user while having a business objective in mind. We employ best practice web design principles to build websites that are aesthetically pleasing.</p>
 							</div>
 						</div>
-						<div class="holder-section">
-							<h3>Video Editing</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+						<div class="col-md-4">
+							<div class="services-grid">
+								<i class="icon-stats-bars2"></i>
+								<h3>Digital Marketing</h3>
+								<p>Our senior strategy consultants will help you refine your target market and their needs, clearly articulate your value proposition, and then identify the right marketing channels to reach your target customers.</p>
+							</div>
 						</div>
-					</div>
-				</div>
-
-				<div class="col-md-4 text-center">
-					<div class="fhco-hero2">
-						<img class="img-responsive" src="images/iphone6.png" alt="iphone6">
-					</div>
-				</div>
-
-				<div class="col-md-4 col-sm-6">
-					<div class="fh5co-services">
-						<div class="fh5co-table2 fh5co-table2-color-4 text-center">
-							<div class="fh5co-table-cell2">
+						<div class="col-md-4">
+							<div class="services-grid">
 								<i class="icon-mobile"></i>
+								<h3>Mobile Optimization</h3>
+								<p>Designing and coding for mobile is essential in our world today. Google penalizes website that haven’t made the jump. Every website we build is responsive, period.</p>
 							</div>
 						</div>
-						<div class="holder-section">
-							<h3>Mobile Optimization</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
-						</div>
-					</div>
-					<div class="fh5co-services">
-						<div class="fh5co-table2 fh5co-table2-color-5 text-center">
-							<div class="fh5co-table-cell2">
+						<div class="col-md-4">
+							<div class="services-grid">
 								<i class="icon-gears"></i>
+								<h3>Search Engine Optimization</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts far from the countries Vokalia and Consonantia.</p>
 							</div>
 						</div>
-						<div class="holder-section">
-							<h3>SEO</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+						<div class="col-md-4">
+							<div class="services-grid">
+								<i class="icon-piechart"></i>
+								<h3>Strategy & Analytics</h3>
+								<p>We want to help you succeed.</br> In order to do that we ask a lot of questions and make sure our strategy meets the trajectory of your organization.</p>
+							</div>
 						</div>
 					</div>
-					<div class="fh5co-services">
-						<div class="fh5co-table2 fh5co-table2-color-6 text-center">
-							<div class="fh5co-table-cell2">
-								<i class="icon-piechart"></i>
+					<!-- <div class="heading-section text-center">
+						<h2>Our Services</h2>
+					</div>
+					<div class="row"> -->
+						<!-- <div class="col-md-4 col-sm-6">
+							<div class="fh5co-services-right">
+								<div class="fh5co-table2 text-center">
+									<div class="fh5co-table-cell2">
+										<i class="icon-heart3"></i>
+									</div>
+								</div>
+								<div class="holder-section">
+									<h3>Crafted With Love</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+								</div>
+							</div>
+							<div class="fh5co-services-right">
+								<div class="fh5co-table2 fh5co-table2-color-2 text-center">
+									<div class="fh5co-table-cell2">
+										<i class="icon-laptop"></i>
+									</div>
+								</div>
+								<div class="holder-section">
+									<h3>Web Design</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+								</div>
+							</div>
+							<div class="fh5co-services-right">
+								<div class="fh5co-table2 fh5co-table2-color-3 text-center">
+									<div class="fh5co-table-cell2">
+										<i class="icon-video"></i>
+									</div>
+								</div>
+								<div class="holder-section">
+									<h3>Video Editing</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+								</div>
 							</div>
 						</div>
-						<div class="holder-section">
-							<h3>Web Analytic</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+
+						<div class="col-md-4 text-center">
+							<div class="fhco-hero2">
+								<img class="img-responsive" src="images/iphone6.png" alt="iphone6">
+							</div>
 						</div>
+
+						<div class="col-md-4 col-sm-6">
+							<div class="fh5co-services">
+								<div class="fh5co-table2 fh5co-table2-color-4 text-center">
+									<div class="fh5co-table-cell2">
+										<i class="icon-mobile"></i>
+									</div>
+								</div>
+								<div class="holder-section">
+									<h3>Mobile Optimization</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+								</div>
+							</div>
+							<div class="fh5co-services">
+								<div class="fh5co-table2 fh5co-table2-color-5 text-center">
+									<div class="fh5co-table-cell2">
+										<i class="icon-gears"></i>
+									</div>
+								</div>
+								<div class="holder-section">
+									<h3>SEO</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+								</div>
+							</div>
+							<div class="fh5co-services">
+								<div class="fh5co-table2 fh5co-table2-color-6 text-center">
+									<div class="fh5co-table-cell2">
+										<i class="icon-piechart"></i>
+									</div>
+								</div>
+								<div class="holder-section">
+									<h3>Web Analytic</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+								</div>
+							</div>
+						</div> -->
 					</div>
 				</div>
 			</div>
