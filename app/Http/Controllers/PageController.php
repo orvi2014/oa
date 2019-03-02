60e012fcd35a4354dfc956ab65e251f2a8b94a58
Himadri Chowdhury <himadrichowdhury@Himadris-MacBook-Pro.local>
2019-03-03 01:17:00 +0600

    issue 15 solved

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -18,5 +19,23 @@ class PageController extends Controller
     }
     public function getWork(){
       return view('work');
+    }
+    public function sendEmail(Request $request){
+        
+      $this->validate($request,[
+        'name'=>'required',
+        'email'=>'required|email',
+        'message'=>'required'
+      ]);
+
+      Mail::send('email.contact-message',
+      [
+        'msg'=>$request->message,
+         'email'=>$request->email
+      ], function($mail) use($request){
+         $mail->from($request->email, $request->name);
+         $mail->to('services.oabd@gmail.com')->subject('O.A Contact Message');
+      });
+      return redirect()->back()->with('flash_message','Thank You for your message, O.A team will contact with you soon');
     }
 }
