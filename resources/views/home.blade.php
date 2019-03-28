c04e923a95b81089c1d1dc64119a949f91362429
sifatul <sifii2013@gmail.com>
2019-03-28 21:59:35 +0900

    contact us all items are aligned

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -456,10 +456,10 @@
                 <div class="col-md-4 animate-box">
                     <h3>Our Address</h3>
                     <ul class="contact-info">
-                        <li><i class="icon-location-pin"></i>Mirpur DOHS, Avenue 3</li>
-                        <li><i class="icon-phone2"></i>+ 0168 3754716</li>
-                        <li><i class="icon-mail"></i><a href="#">services.oabd@gmail.com</a></li>
-                        <li><i class="icon-globe2"></i><a href="#">oabd.herokuapp.com</a></li>
+						<li><i class="icon-location-pin"></i><p>Mirpur DOHS, Avenue 3</p></li>
+                        <li><i class="icon-phone2"></i> <p>+880 168 3754716</p></li>
+                        <li><i class="icon-mail"></i><p><a href="#">services.oabd@gmail.com </a></p></li>
+                        <li><i class="icon-globe2"></i><p><a href="#">oabd.herokuapp.com</a></p></li>
                     </ul>
                 </div>
 
@@ -484,8 +484,12 @@
                                 <div class="form-group">
                                     <textarea name="message" class="form-control" id="" cols="30" rows="7" placeholder="Message"></textarea>
                                 </div>
+							</div>
+							<div class="col-md-6">
+                                <div class="form-group">
+								<button class="btn btn-primary">Send Message</button> </div>
                             </div>
-                            <button class="btn btn-primary">Send Message</button>
+                            
                         </form>
                     </div>
                 </div>
