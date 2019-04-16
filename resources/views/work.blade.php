cbe8114ae97ec4826bae0dffd1662de425b0a4a0
lordofxtreme <kingfahad0802@gmail.com>
2019-04-16 22:38:48 +0600

    Our Work 40% complete

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -9,11 +9,11 @@
 				<div class="fh5co-grid-work">
 					<div class="work-holder">
 						<a href="#"><img src="{{url('/img/OAERP.png')}}" alt="Work"></a>
-						<a href="#" class="inner-overlay"><i class="icon-plus"></i></a>
+						<a href="#" class="inner-overlay"><i class="icon-plus"></i>Edit here</a>
 					</div>
 					<div class="desc">
-						<h3><a href="#">ERP Software</a></h3>
-						<span></span>
+					<span><h3><a href="#">ERP Software</a></h3>
+						</span>
 					</div>
 				</div>
 			</div>
@@ -22,12 +22,12 @@
 			<div class="col-md-6 col-sm-6">
 				<div class="fh5co-grid-work">
 					<div class="work-holder">
-						<a href="#"><img src="{{url('/img/library.jpg')}}" alt="Work"></a>
-						<a href="#" class="inner-overlay inner-overlay-2"><i class="icon-plus"></i></a>
+						<a href="#"><img src="{{url('/img/library.png')}}" alt="Work"></a>
+						<a href="#" class="inner-overlay inner-overlay-2"><i class="icon-plus"></i> Edit here</a>
 					</div>
 					<div class="desc">
-						<h3><a href="#">Library Management Software</a></h3>
-						<span></span>
+					<span><h3><a href="#">Library Management Software</a></h3>
+						</span>
 					</div>
 				</div>
 			</div>
@@ -38,12 +38,12 @@
 			<div class="col-md-6 col-sm-6">
 				<div class="fh5co-grid-work">
 					<div class="work-holder">
-						<a href="#"><img src="{{url('/img/Screen Shot 2019-03-27 at 11.06.20 PM.png')}}" alt="Work"></a>
-						<a href="#" class="inner-overlay inner-overlay-3"><i class="icon-plus"></i></a>
+						<a href="#"><img src="{{url('/img/inventorymanagement.png')}}" alt="Work"></a>
+						<a href="#" class="inner-overlay inner-overlay-3"><i class="icon-plus"></i>Edit here</a>
 					</div>
 					<div class="desc">
-						<h3><a href="#">Inventory Management System</a></h3>
-						<span></span>
+					<span><h3><a href="#">Inventory Management System</a></h3>
+						</span>
 					</div>
 				</div>
 			</div>
@@ -52,12 +52,12 @@
 			<div class="col-md-6 col-sm-6">
 				<div class="fh5co-grid-work">
 					<div class="work-holder">
-						<a href="#"><img src="{{url('/img/Screenshot_2019-03-26 Demo LMS -.png')}}" alt="Work"></a>
-						<a href="#" class="inner-overlay inner-overlay-4"><i class="icon-plus"></i></a>
+						<a href="#"><img src="{{url('/img/e-learning.png')}}" alt="Work"></a>
+						<a href="#" class="inner-overlay inner-overlay-4"><i class="icon-plus"></i>Edit here</a>
 					</div>
 					<div class="desc">
-						<h3><a href="#">E-Learning Platform</a></h3>
-						<span></span>
+					<span><h3><a href="#">E-Learning Platform</a></h3>
+						</span>
 					</div>
 				</div>
 			</div>
