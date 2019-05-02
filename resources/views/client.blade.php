586a5a6de0405e1731c6bee4b8a388b01f3db047
Himadri101 <himadrichowdhury172@gmail.com>
2019-05-03 01:06:55 +0600

    client image height & width modified

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -6,23 +6,18 @@
         <div class="owl-carousel owl-carousel2">
             <div class="item">
                 <!-- <a href="#" class="image-popup"> -->
-                <img style="height: 100%; width: 80%; object-fit: contain" src="{{ url('public/img/square_banner.jpg') }}" alt="image" />
+                <img style="height: 80%; width: 60%; object-fit: contain" src="{{ url('/img/square_banner.jpg') }}" alt="image" />
                 <!-- </a> -->
 
             </div>
             <div class="item">
                 <!-- <a href="#" class="image-popup"> -->
-                <img style="height: 100%; width: 80%; object-fit: contain" src="{{url('/img/army.jpg')}}" alt="image" />
+                <img style="height: 80%; width: 60%; object-fit: contain" src="{{url('/img/army.jpg')}}" alt="image" />
                 <!-- </a> -->
             </div>
             <div class="item">
                 <!-- <a href="#" class="image-popup"> -->
-                <img style="height: 100%; width: 80%; object-fit: contain" src="{{url('public/img/shahmiran_logo.png')}}" alt="image" />
-                <!-- </a> -->
-            </div>
-            <div class="item">
-                <!-- <a href="#" class="image-popup"> -->
-                <img style="height: 100%; width: 80%; object-fit: contain" src="{{url('public/img/meridian_logo.jpeg')}}" alt="image" />
+                <img style="height: 80%; width: 60%; object-fit: contain" src="{{url('/img/shahmiran_logo.png')}}" alt="image" />
                 <!-- </a> -->
             </div>
         </div>
